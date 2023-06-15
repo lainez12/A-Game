0f38afb775037013147fbdb9d48e7ecb06430b62
Will Lainez <129980672+lainez12@users.noreply.github.com>
2023-06-15 13:41:16 +0200

    La seleción aleatoria del enemigo

--- js/javascript.js
+++ js/javascript.js
@@ -17,6 +17,24 @@ function selecionMascota() {
     } else {
         alert("No selecionaste nada, selecion tu mascota")
     }
+
+    mascotaEnemigo()
+}
+
+function mascotaEnemigo() 
+{
+    let mascotaAleatoria = aletorio(1,3)
+    let mascotaEnemigo = document.getElementById('mascota-enemigo')
+    if (mascotaAleatoria == 1) {
+        mascotaEnemigo.innerHTML = 'Hipodege'
+    } else if (mascotaAleatoria == 2) {
+        mascotaEnemigo.innerHTML = 'Capipepo'
+    } else if (mascotaAleatoria == 3) {
+        mascotaEnemigo.innerHTML = 'Ratigueya' }
+}
+
+function aletorio(min, max) {
+    return Math.floor(Math.random()*(max-min+1)+min)
 }
 
 window.addEventListener('load', iniciarjuego)
