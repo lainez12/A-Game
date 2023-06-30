84124410575ab703762c301bfe43fcbcc519ba65
lainez12 <2004lainez@gmail.com>
2023-06-30 17:08:06 +0200

    Puse las variables principales de Get Id afuera

--- js/javascript.js
+++ js/javascript.js
@@ -1,85 +1,90 @@
+let botonMascotaJugador = document.getElementById('boton-mascota')
+let botonFuego = document.getElementById('boton-fuego')
+let botonAgua = document.getElementById('boton-agua')
+let botonTierra = document.getElementById('boton-tierra')
+let ocultarAtaque = document.getElementById('selecionar-ataque')
+let ocultarReinicio = document.getElementById('reinicio')
+let botonReiniciar = document.getElementById('boton-reiniciar')
+
+let hipodege = document.getElementById('hipodege')
+let capipepo = document.getElementById('capipepo')
+let ratigueya = document.getElementById('ratigueya')
+let mascotaSelecionada = document.getElementById('mascota-selecionada')
+let imagenMascota = document.getElementById('imgAnimal')
+
+let ocultarMascota = document.getElementById('selecionar-mascota')
+
+let mascotaEnemigo = document.getElementById('mascota-enemigo')
+let imagenMascotaJugador = document.getElementById('imgEnemigo')
+
+let spanVidasJugador = document.getElementById('vidas-jugador')
+let spanVidasEnemigo = document.getElementById('vidas-enemigo')
+
+let resultadoBatalla = document.getElementById('resultados')
+let tuAtaque = document.getElementById('name-Ataque-Jugador')
+let elAtaqueEnemigo = document.getElementById('name-Ataque-Enemigo')
+
 let ataqueJugador
 let ataqueRival
 let vidasJugador = 3
 let vidasEnemigo = 3
 
 function iniciarjuego() {
-    let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', selecionMascota)
 
-    let botonFuego = document.getElementById('boton-fuego')
     botonFuego.addEventListener('click', ataqueFuego)
 
-    let botonAgua = document.getElementById('boton-agua')
     botonAgua.addEventListener('click', ataqueAgua)
     
-    let botonTierra = document.getElementById('boton-tierra')
     botonTierra.addEventListener('click', ataqueTierra)
 
-    let ocultarAtaque = document.getElementById('selecionar-ataque')
     ocultarAtaque.style.display = 'none'
 
-    let ocultarReinicio = document.getElementById('reinicio')
     ocultarReinicio.style.display = 'none'
 
-    let botonReiniciar = document.getElementById('boton-reiniciar')
     botonReiniciar.addEventListener('click', reiniciarJuego)
 }
 
 function selecionMascota() {
-    let hipodege = document.getElementById('hipodege')
-    let capipepo = document.getElementById('capipepo')
-    let ratigueya = document.getElementById('ratigueya')
-    let mascotaSelecionada = document.getElementById('mascota-selecionada')
-    let imagenMascota = document.getElementById('imgAnimal')
     if (hipodege.checked == true) {
-        let ocultarMascota = document.getElementById('selecionar-mascota')
         ocultarMascota.style.display = 'none'
 
-        let ocultarAtaque = document.getElementById('selecionar-ataque')
         ocultarAtaque.style.display = 'block'
         
         mascotaSelecionada.innerHTML = 'Hipodoge'
         imagenMascota.setAttribute("src", "https://simp6.jpg.church/images/Criatura1ef2057ad22a11bf7.png");
-        mascotaEnemigo()
+        mascotaEnemigoSelecionar()
     } else if (capipepo.checked == true) {
-        let ocultarMascota = document.getElementById('selecionar-mascota')
         ocultarMascota.style.display = 'none'
 
-        let ocultarAtaque = document.getElementById('selecionar-ataque')
         ocultarAtaque.style.display = 'block'
         
         mascotaSelecionada.innerHTML = 'Capipepo'
         imagenMascota.setAttribute("src", "https://simp6.jpg.church/images/Criatura2b21aadb89e39dc36.png");
-        mascotaEnemigo()
+        mascotaEnemigoSelecionar()
     } else if (ratigueya.checked == true) {
-        let ocultarMascota = document.getElementById('selecionar-mascota')
         ocultarMascota.style.display = 'none'
 
-        let ocultarAtaque = document.getElementById('selecionar-ataque')
         ocultarAtaque.style.display = 'block'
         mascotaSelecionada.innerHTML = 'Ratigueya'
         imagenMascota.setAttribute("src", "https://simp6.jpg.church/images/Criatura38867b5557f90ae32.png")
-        mascotaEnemigo()
+        mascotaEnemigoSelecionar()
     } else {
         alert("No selecionaste nada, seleciona tu mascota")
     }
 }
 
-function mascotaEnemigo() 
-{
+function mascotaEnemigoSelecionar() {
     let mascotaAleatoria = aletorio(1,3)
-    let mascotaEnemigo = document.getElementById('mascota-enemigo')
-    let imagenMascota = document.getElementById('imgEnemigo')
     if (mascotaAleatoria == 1) {
         mascotaEnemigo.innerHTML = 'EL Hipodege'
-        imagenMascota.setAttribute("src", "https://simp6.jpg.church/images/Criatura1ef2057ad22a11bf7.png");
+        imagenMascotaJugador.setAttribute("src", "https://simp6.jpg.church/images/Criatura1ef2057ad22a11bf7.png");
     } else if (mascotaAleatoria == 2) {
         mascotaEnemigo.innerHTML = 'EL Capipepo'
-        imagenMascota.setAttribute("src", "https://simp6.jpg.church/images/Criatura2b21aadb89e39dc36.png");
+        imagenMascotaJugador.setAttribute("src", "https://simp6.jpg.church/images/Criatura2b21aadb89e39dc36.png");
     } else if (mascotaAleatoria == 3) {
         mascotaEnemigo.innerHTML = 'La Ratigueya'
-        imagenMascota.setAttribute("src", "https://simp6.jpg.church/images/Criatura38867b5557f90ae32.png") }
+        imagenMascotaJugador.setAttribute("src", "https://simp6.jpg.church/images/Criatura38867b5557f90ae32.png") }
 }
 
 function ataqueFuego() {
@@ -112,8 +117,6 @@ function ataqueEnemigo() {
 }
 
 function combate(jugador, pc) {
-    let spanVidasJugador = document.getElementById('vidas-jugador')
-    let spanVidasEnemigo = document.getElementById('vidas-enemigo')
     
     if (jugador == pc) {
         mensajeJuego('Empate')    
@@ -133,11 +136,9 @@ function combate(jugador, pc) {
 function revisarVidas() {
     if (vidasEnemigo == 0) {
         mensajeFinal('Deberias sentirte mal mataste una mascota virtual')
-        let ocultarReinicio = document.getElementById('reinicio')
         ocultarReinicio.style.display = 'block'
     } else if (vidasJugador == 0) {
         mensajeFinal('Deberias sentirte mal una pc te gano')
-        let ocultarReinicio = document.getElementById('reinicio')
         ocultarReinicio.style.display = 'block'
     } else {
 
@@ -145,24 +146,15 @@ function revisarVidas() {
 }
 
 function mensajeJuego(resultado) {
-    let resultadoBatalla = document.getElementById('resultados')
-    let tuAtaque = document.getElementById('name-Ataque-Jugador')
-    let elAtaqueEnemigo = document.getElementById('name-Ataque-Enemigo')
     resultadoBatalla.innerHTML = resultado
     tuAtaque.innerHTML = ataqueJugador
     elAtaqueEnemigo.innerHTML = ataqueRival
 }
 
 function mensajeFinal(mensaje) {
-    let resultadoBatalla = document.getElementById('ganadorMensaje')
-    let tuAtaque = document.getElementById('name-Ataque-Jugador')
-    let elAtaqueEnemigo = document.getElementById('name-Ataque-Enemigo')
     resultadoBatalla.innerHTML = mensaje
     tuAtaque.innerHTML = ' '
     elAtaqueEnemigo.innerHTML = ' '
-    let botonFuego = document.getElementById('boton-fuego')
-    let botonAgua = document.getElementById('boton-agua')
-    let botonTierra = document.getElementById('boton-tierra')
     botonFuego.disabled = true
     botonAgua.disabled = true
     botonTierra.disabled = true
